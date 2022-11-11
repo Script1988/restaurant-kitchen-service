06accdde0330a9aae08b82d9877206d4c809785c
Volodymyr Panov <vpanov25@gmail.com>
2022-11-11 22:43:43 +0100

    Add restaurant_service to INSTALLED_APPS

--- restaurant_service/apps.py
+++ restaurant_service/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class RestaurantServiceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'restaurant_service'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "restaurant_service"
