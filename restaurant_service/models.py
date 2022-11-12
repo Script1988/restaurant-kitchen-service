d86f59e5f7b4d9710a2a9bad013e82712f7f5b04
Volodymyr Panov <vpanov25@gmail.com>
2022-11-12 12:53:55 +0100

    add dish and dish_type models to models.py

--- restaurant_service/models.py
+++ restaurant_service/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Cook(AbstractUser):
+    pass
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    dish_type = models.ForeignKey(
+        DishType,
+        on_delete=models.CASCADE,
+        related_name="dish_type"
+    )
+    cooks = models.ManyToManyField(Cook, related_name="cook")
+
+    def __str__(self):
+        return f"{self.name}: {self.price}"
