4ed633942a2bad35ed9ac7b561721443275250a3
Volodymyr Panov <vpanov25@gmail.com>
2022-11-12 20:13:19 +0100

    Add and customizing admin panel

--- restaurant_service/models.py
+++ restaurant_service/models.py
@@ -7,17 +7,20 @@ class DishType(models.Model):
     name = models.CharField(max_length=255)
 
     class Meta:
-        ordering = "name"
+        ordering = ["name"]
 
     def __str__(self):
         return self.name
 
 
 class Cook(AbstractUser):
-    years_of_experience = models.IntegerField()
+    years_of_experience = models.IntegerField(null=True)
 
     class Meta:
-        ordering = "username"
+        ordering = ["username"]
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
 
 
 class Dish(models.Model):
