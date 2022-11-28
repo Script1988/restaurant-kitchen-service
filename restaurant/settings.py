ad1fb105c118d97e7e297f1bc492bb2e75d9c814
Volodymyr Panov <vpanov25@gmail.com>
2022-11-28 18:15:48 +0100

    Add files to gitignore and prepare settings for new database

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+
 import os
 from pathlib import Path
+
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,11 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-b*pa!^r%x1x+!$psx_-cupdalvgc%%sva=@h$plh7wg29=e8xy"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-b*pa!^r%x1x+!$psx_-cupdalvgc%%sva=@h$plh7wg29=e8xy")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1",]
 INTERNAL_IPS = ["127.0.0.1"]
 
 # Application definition
@@ -87,6 +91,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
